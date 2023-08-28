118139e0a3cb02f2cd2d9da41e0110d95396e4e2
Kallyne Layane <hellookallyne@gmail.com>
2023-08-27 21:31:51 -0300

    Add Board module

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,1 +1,18 @@
 const Player = (name, mark) => ({ name, mark });
+
+const Board = (() => {
+  let board = Array(9).fill('');
+
+  const getBoard = () => board;
+
+  const reset = () => (board = Array(9).fill(''));
+
+  const isFull = () => board.every(square => square !== '');
+
+  const markSquare = squareId => {
+    board[squareId] = Game.getCurrentPlayer().mark;
+    Game.switchPlayer();
+  };
+
+  return { getBoard, markSquare, isFull, reset };
+})();
